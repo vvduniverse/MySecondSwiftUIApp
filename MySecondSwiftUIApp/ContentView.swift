654acf540de64bd1b41311a566dcc7cbfb31d9e3
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-10-20 18:07:58 +0300

    more code snippets

--- MySecondSwiftUIApp/ContentView.swift
+++ MySecondSwiftUIApp/ContentView.swift
@@ -9,26 +9,36 @@ import SwiftUI
 
 
 struct ContentView: View {
+    
+    @State private var showingAlert1 = false
+    @State private var showingAlert2 = false
+    @State private var showingAlert3 = false
+    
     var body: some View {
-        NavigationView{
-            Text("Learning SwiftUI")
-                .navigationBarTitle("Welcome", displayMode: .inline)
-                .navigationBarItems(leading:
-                                        HStack {
-                                            Button("Edit") {
-                                                print("Button edit tapped")
-                                            }
-                                            Button("Cancel") {
-                                                print("Button cancel tapped")
-                                            }
-                                        }
-                )
+        HStack {
+            Button("Show Alert 1") {
+                self.showingAlert1 = true
+            } .alert(isPresented: $showingAlert1) {
+                Alert(title: Text("HelloSwiftUI 1"),
+                      message: Text("Some detail message 1"),
+                      dismissButton: .default(Text("OK 1")))
+            }
+            Button("Show Alert 2") {
+                self.showingAlert2 = true
+            } .alert(isPresented: $showingAlert2) {
+                Alert(title: Text("HelloSwiftUI 2"),
+                      message: Text("Some detail message 2"),
+                      dismissButton: .cancel() )
+            }
+            Button("Show 3") {
+                self.showingAlert3 = true
+            } .alert(isPresented: $showingAlert3) {
+                Alert(title: Text("HelloSwiftUI 3"),
+                      dismissButton: .default(Text("OK3")) )
+            }
         }
     }
 }
-
-
-
 
 
 
--- MySecondSwiftUIApp/Examples.swift
+++ MySecondSwiftUIApp/Examples.swift
@@ -124,11 +124,139 @@
      }
  }
  *************************************
+ struct ContentView: View {
+     
+     @State private var users = ["Nadiia", "Anna", "Vitalii", "Sofia",]
+     
+     var body: some View {
+         NavigationView{
+             List {
+                 ForEach(users, id: \.self) {user in
+                     Text(user)
+                 } .onDelete(perform: delete)
+             }
+         }
+     }
+     
+     func delete(at offset: IndexSet) {
+         users.remove(atOffsets: offset)
+     }
+ }
  *************************************
+ struct ContentView: View {
+     
+     @State private var users = ["Nadiia", "Anna", "Vitalii", "Sofia",]
+     
+     var body: some View {
+         NavigationView{
+             List {
+                 ForEach(users, id: \.self) {user in
+                     Text(user)
+                 } .onMove(perform: move)
+             }
+             .navigationBarItems(trailing: EditButton())
+         }
+     }
+     
+     func move(from source: IndexSet, to destination: Int) {
+         users.move(fromOffsets: source, toOffset: destination)
+     }
+ }
  *************************************
+ struct ContentView: View {
+     
+     @State private var selectedView = 1
+     
+     var body: some View {
+         TabView(selection: $selectedView) {
+             Text("Bus View")
+                 .tabItem {
+                     Image(systemName: "bus")
+                     Text("BUS") }.tag(1)
+             Text("Car View")
+                 .tabItem {
+                     Image(systemName: "car")
+                     Text("CAR") }.tag(2)
+         }
+         
+     }
+ }
  *************************************
+ struct ContentView: View {
+     
+     @State private var cars = ["Tesla", "Audi", "BMW", "Porsche"]
+     @State private var selectedCar = 0
+     @State private var aditionalSettings = false
+     
+     
+     var body: some View {
+         NavigationView {
+             Form {
+                 Section(header: Text("Cars")) {
+                     Picker(selection: $selectedCar, label: Text("Select a car")) {
+                         ForEach(0..<cars.count) {
+                             Text(self.cars[$0])
+                         }
+                     } .pickerStyle(SegmentedPickerStyle())
+                 }
+                 Toggle(isOn: $aditionalSettings) {
+                     Text("Aditional settings")
+                 }
+                 Button(action: {}) {
+                     Text("Save changes")
+                 } .disabled(!aditionalSettings)
+             } .navigationBarTitle("Cars yopt!")
+         }
+     }
+ }
+
  *************************************
+ struct ContentView: View {
+     
+     @State private var showingAlert = false
+     
+     var body: some View {
+         Button("Show Alert") {
+             self.showingAlert = true
+         } .alert(isPresented: $showingAlert) {
+             Alert(title: Text("HelloSwiftUI"),
+                   message: Text("Some detail message"),
+                   dismissButton: .default(Text("OK")))
+         }
+     }
+ }
  *************************************
+ struct ContentView: View {
+     
+     @State private var showingAlert1 = false
+     @State private var showingAlert2 = false
+     @State private var showingAlert3 = false
+     
+     var body: some View {
+         HStack {
+             Button("Show Alert 1") {
+                 self.showingAlert1 = true
+             } .alert(isPresented: $showingAlert1) {
+                 Alert(title: Text("HelloSwiftUI 1"),
+                       message: Text("Some detail message 1"),
+                       dismissButton: .default(Text("OK 1")))
+             }
+             Button("Show Alert 2") {
+                 self.showingAlert2 = true
+             } .alert(isPresented: $showingAlert2) {
+                 Alert(title: Text("HelloSwiftUI 2"),
+                       message: Text("Some detail message 2"),
+                       dismissButton: .cancel() )
+             }
+             Button("Show 3") {
+                 self.showingAlert3 = true
+             } .alert(isPresented: $showingAlert3) {
+                 Alert(title: Text("HelloSwiftUI 3"),
+                       dismissButton: .default(Text("OK3")) )
+             }
+         }
+     }
+ }
  *************************************
  *************************************
  

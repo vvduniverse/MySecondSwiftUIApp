bc4b2e7763dcffd3d6ce07fccdd3ea012f22ac9f
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-10-20 10:52:20 +0300

    Initial Commit

---
+++ MySecondSwiftUIApp/MySecondSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySecondSwiftUIAppApp.swift
+//  MySecondSwiftUIApp
+//
+//  Created by Vahtee Boo on 20.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MySecondSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
